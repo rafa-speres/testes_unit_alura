9e2e0803a4cb1540dde42e94b19637d445a74495
rafa-speres <rafa.scalon@gmail.com>
2023-02-17 17:18:41 -0300

    fim dos testes de folha

--- test/folhaDePagamento.test.js
+++ test/folhaDePagamento.test.js
@@ -1,15 +1,17 @@
 import { somaHorasExtras, calculaDescontos } from "../index.js";
 
-test('Deve retornar a soma das horas extras', () =>{
-    const esperado = 2500;
-    const retornado = somaHorasExtras(2000, 500);
-
-    expect(retornado).toBe(esperado);
-});
-
-test('Deve retornar a subtracao das horas extras', () => {
-    const esperado = 2200;
-    const retornado = calculaDescontos(2500, 300);
-
-    expect(retornado).toBe(esperado);
+describe('Teste dos calculos de folha', () =>{
+    it('Deve retornar a soma das horas extras', () =>{
+        const esperado = 2500;
+        const retornado = somaHorasExtras(2000, 500);
+    
+        expect(retornado).toBe(esperado);
+    });
+    
+    it('Deve retornar a subtracao das horas extras', () => {
+        const esperado = 2200;
+        const retornado = calculaDescontos(2500, 300);
+    
+        expect(retornado).toBe(esperado);
+    });
 });
